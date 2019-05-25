85816f56703778922a84c3292e77353befd45e49
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2019-05-25 19:09:07 +0200

    Basic UI designed

---
+++ CTester/src/constants/Constants.java
@@ -0,0 +1,20 @@
+package constants;
+
+/**
+ *
+ * @author dma@logossmartcard.com
+ */
+public class Constants {
+    
+    // GUI Constants
+    public static int ICON_WIDTH  = 28;
+    public static int ICON_HEIGHT = 28;
+    
+    // Paths
+    public static String RESOURCES_PATH = "/resources/";
+    
+    // File names
+    public static String RUN_ICON      = "arrow-right.png";
+    public static String COMPILE_ICON  = "code-download.png";
+    public static String NEW_TEST_ICON = "file-add.png";
+}
