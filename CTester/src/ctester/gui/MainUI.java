f25ac22bf27453c95610ae3376b08a390da3dca3
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2019-05-30 22:35:41 +0200

    Some command buttons added

--- CTester/src/ctester/gui/MainUI.java
+++ CTester/src/ctester/gui/MainUI.java
@@ -172,6 +172,12 @@ public class MainUI extends javax.swing.JFrame
         jPanel3 = new javax.swing.JPanel();
         mRoundCommandsIcon = new javax.swing.JLabel();
         mRoundCommands = new javax.swing.JLabel();
+        mVirginizeButton = new javax.swing.JPanel();
+        jLabel5 = new javax.swing.JLabel();
+        mSecHistoryButton = new javax.swing.JPanel();
+        jLabel6 = new javax.swing.JLabel();
+        mBootloaderButton = new javax.swing.JPanel();
+        jLabel7 = new javax.swing.JLabel();
         mLeftPanel = new javax.swing.JPanel();
         jPanel1 = new javax.swing.JPanel();
         mCompileButton = new javax.swing.JButton();
@@ -241,6 +247,7 @@ public class MainUI extends javax.swing.JFrame
         jLabel4.setBackground(new java.awt.Color(51, 51, 51));
         jLabel4.setFont(new java.awt.Font("Microsoft YaHei UI Light", 1, 12)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(51, 51, 51));
+        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel4.setText("Send");
         mSendButton.add(jLabel4);
 
@@ -263,7 +270,7 @@ public class MainUI extends javax.swing.JFrame
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel4Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(mCommandEdittext, javax.swing.GroupLayout.PREFERRED_SIZE, 267, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(mCommandEdittext, javax.swing.GroupLayout.PREFERRED_SIZE, 320, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(mSendButton, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
@@ -297,6 +304,42 @@ public class MainUI extends javax.swing.JFrame
 
         mRightToolbarLowerPanel.add(jPanel3, new java.awt.GridBagConstraints());
 
+        mVirginizeButton.setBackground(new java.awt.Color(94, 237, 181));
+        mVirginizeButton.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(94, 237, 181), 2, true));
+        mVirginizeButton.setToolTipText("Generate virginize command");
+        mVirginizeButton.setOpaque(false);
+
+        jLabel5.setBackground(new java.awt.Color(204, 204, 204));
+        jLabel5.setFont(new java.awt.Font("Microsoft YaHei UI Light", 1, 12)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(204, 204, 204));
+        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel5.setText("Virginize");
+        mVirginizeButton.add(jLabel5);
+
+        mSecHistoryButton.setBackground(new java.awt.Color(94, 237, 181));
+        mSecHistoryButton.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(94, 237, 181), 2, true));
+        mSecHistoryButton.setToolTipText("Generate Get Security History command");
+        mSecHistoryButton.setOpaque(false);
+
+        jLabel6.setBackground(new java.awt.Color(204, 204, 204));
+        jLabel6.setFont(new java.awt.Font("Microsoft YaHei UI Light", 1, 12)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(204, 204, 204));
+        jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel6.setText("Sec History");
+        mSecHistoryButton.add(jLabel6);
+
+        mBootloaderButton.setBackground(new java.awt.Color(94, 237, 181));
+        mBootloaderButton.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(94, 237, 181), 2, true));
+        mBootloaderButton.setToolTipText("Generate Back to Bootloader command");
+        mBootloaderButton.setOpaque(false);
+
+        jLabel7.setBackground(new java.awt.Color(204, 204, 204));
+        jLabel7.setFont(new java.awt.Font("Microsoft YaHei UI Light", 1, 12)); // NOI18N
+        jLabel7.setForeground(new java.awt.Color(204, 204, 204));
+        jLabel7.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel7.setText("Bootloader");
+        mBootloaderButton.add(jLabel7);
+
         javax.swing.GroupLayout mRightPanelLayout = new javax.swing.GroupLayout(mRightPanel);
         mRightPanel.setLayout(mRightPanelLayout);
         mRightPanelLayout.setHorizontalGroup(
@@ -314,7 +357,13 @@ public class MainUI extends javax.swing.JFrame
                             .addComponent(jLabel2)
                             .addComponent(mOutputScrollPane, javax.swing.GroupLayout.PREFERRED_SIZE, 705, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(mRightToolbarLowerPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGroup(mRightPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, mRightPanelLayout.createSequentialGroup()
+                                .addComponent(mRightToolbarLowerPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(3, 3, 3))
+                            .addComponent(mVirginizeButton, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(mSecHistoryButton, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(mBootloaderButton, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addContainerGap())
         );
         mRightPanelLayout.setVerticalGroup(
@@ -333,6 +382,12 @@ public class MainUI extends javax.swing.JFrame
                     .addComponent(mOutputScrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 630, Short.MAX_VALUE)
                     .addGroup(mRightPanelLayout.createSequentialGroup()
                         .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(mBootloaderButton, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(mSecHistoryButton, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(mVirginizeButton, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(mRightToolbarLowerPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap())
         );
@@ -525,11 +580,15 @@ public class MainUI extends javax.swing.JFrame
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JButton mAddButton;
+    private javax.swing.JPanel mBootloaderButton;
     private javax.swing.JTextField mCommandEdittext;
     private javax.swing.JButton mCompileButton;
     private javax.swing.JPanel mLeftPanel;
@@ -542,11 +601,13 @@ public class MainUI extends javax.swing.JFrame
     private javax.swing.JLabel mRoundCommands;
     private javax.swing.JLabel mRoundCommandsIcon;
     private javax.swing.JButton mRunButton;
+    private javax.swing.JPanel mSecHistoryButton;
     private javax.swing.JCheckBox mSelectAllCheckBox;
     private javax.swing.JPanel mSendButton;
     private javax.swing.JButton mSettingsButton;
     private javax.swing.JSplitPane mSplitPane;
     private javax.swing.JPanel mTestsListPanel;
     private javax.swing.JScrollPane mTestsScrollPane;
+    private javax.swing.JPanel mVirginizeButton;
     // End of variables declaration//GEN-END:variables
 }
