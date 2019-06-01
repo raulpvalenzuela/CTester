b13c702c42acd8988b46d008d42f82521935a1d9
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2019-06-01 17:23:28 +0200

    Scene recreated to be correctly resizable

--- CTesterFX/src/main/java/com/lsc/ctesterfx/controllers/FXMLMainController.java
+++ CTesterFX/src/main/java/com/lsc/ctesterfx/controllers/FXMLMainController.java
@@ -1,19 +1,35 @@
 package com.lsc.ctesterfx.controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.Parent;
+import javafx.scene.layout.VBox;
 
 public class FXMLMainController implements Initializable 
 {
     @FXML
-    private ScrollPane mOutputScrollPane;
+    private VBox mTestListVBox;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) 
     {
-        
-    }    
+        for (int i = 0; i < 15; ++i)
+        {
+            try 
+            {
+                Parent testItem = FXMLLoader.load(getClass().getResource("/fxml/TestItem.fxml"));
+                
+                mTestListVBox.getChildren().add(testItem);
+                
+            } catch (IOException ex) {
+                Logger.getLogger(FXMLMainController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
 }
