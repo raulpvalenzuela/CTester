d7290136a6561d55dffd357b7ede29ab21616fae
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2019-06-02 17:56:22 +0200

    Fade animations when FAB clicked

---
+++ CTesterFX/src/main/java/com/lsc/ctesterfx/constants/Constants.java
@@ -0,0 +1,14 @@
+package com.lsc.ctesterfx.constants;
+
+/**
+ *
+ * @author dma@logossmartcard.com
+ */
+public class Constants 
+{
+    // Animations
+    public static final int FADE_IN_ANIMATION_DURATION  = 100;
+    public static final int FADE_OUT_ANIMATION_DURATION = 100;
+    
+    public static final int FAST_DELAY = 65;
+}
--- CTesterFX/src/main/java/com/lsc/ctesterfx/controllers/FXMLMainController.java
+++ CTesterFX/src/main/java/com/lsc/ctesterfx/controllers/FXMLMainController.java
@@ -2,14 +2,19 @@ package com.lsc.ctesterfx.controllers;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import com.lsc.ctesterfx.constants.Constants;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.FadeTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 import org.fxmisc.flowless.VirtualizedScrollPane;
 import org.fxmisc.richtext.InlineCssTextArea;
 
@@ -20,8 +25,12 @@ import org.fxmisc.richtext.InlineCssTextArea;
  */
 public class FXMLMainController implements Initializable 
 {
+    // Flag to indicate if the commands list is visible.
+    private boolean mCommandsListVisible;
+    
     @FXML
     private VBox mTestListVBox;
+    
     @FXML
     private BorderPane mOutputContainer;
     
@@ -54,6 +63,10 @@ public class FXMLMainController implements Initializable
     // RichTextArea that will contain the output of the test.
     private final InlineCssTextArea mOutputTextArea = new InlineCssTextArea();
     
+    // Add all the animations to be run when the FAB is clicked.
+    private FadeTransition[] mFadeInAnimationsList;    
+    private FadeTransition[] mFadeOutAnimationsList;
+    
     /**
      * Initializes the controller class.
      * @param url
@@ -61,6 +74,25 @@ public class FXMLMainController implements Initializable
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) 
+    {
+        _initialize();
+        _setupOutputArea();
+        _setupTooltips();
+        _setupAnimations();
+    }
+    
+    /**
+     * Sets up different variables.
+     */
+    private void _initialize()
+    {
+        mCommandsListVisible = true;
+    }
+    
+    /**
+     * Sets up the output area.
+     */
+    private void _setupOutputArea()
     {
         // Set the common style for output. Monospace and font size.
         mOutputTextArea.setStyle("-fx-font-family: monospace; -fx-font-size: 10pt;");
@@ -80,5 +112,99 @@ public class FXMLMainController implements Initializable
         vsPane.prefHeightProperty().bind(mOutputContainer.prefHeightProperty());
         
         mOutputContainer.setCenter(vsPane);
+    }
+    
+    /**
+     * Sets up all the buttons' tooltips.
+     */
+    private void _setupTooltips()
+    {
+        mAddTestsButton.setTooltip(new Tooltip("Add new tests"));
+        mFABButton.setTooltip(new Tooltip("Show more commands"));
+    }
+    
+    /**
+     * Sets up all the animations in this window.
+     */
+    private void _setupAnimations()
+    {        
+        mFadeInAnimationsList = new FadeTransition[] { 
+              new FadeTransition(Duration.millis(Constants.FADE_IN_ANIMATION_DURATION), mVirginizeButton)
+            , new FadeTransition(Duration.millis(Constants.FADE_IN_ANIMATION_DURATION), mSecurityHistoryButton)
+            , new FadeTransition(Duration.millis(Constants.FADE_IN_ANIMATION_DURATION), mGetProductCodeButton)
+            , new FadeTransition(Duration.millis(Constants.FADE_IN_ANIMATION_DURATION), mBootloaderButton) 
+        };
+        
+        mFadeOutAnimationsList = new FadeTransition[] {        
+              new FadeTransition(Duration.millis(Constants.FADE_OUT_ANIMATION_DURATION), mVirginizeButton)
+            , new FadeTransition(Duration.millis(Constants.FADE_OUT_ANIMATION_DURATION), mSecurityHistoryButton)
+            , new FadeTransition(Duration.millis(Constants.FADE_OUT_ANIMATION_DURATION), mGetProductCodeButton)
+            , new FadeTransition(Duration.millis(Constants.FADE_OUT_ANIMATION_DURATION), mBootloaderButton) 
+        };
+        
+        int delay = 0;        
+        for (int i = 0; i < mFadeInAnimationsList.length; ++i)
+        {
+            mFadeInAnimationsList[i].setFromValue(0.0f);
+            mFadeInAnimationsList[i].setToValue(1.0f);
+            mFadeInAnimationsList[i].setDelay(Duration.millis(delay));
+
+            delay += Constants.FAST_DELAY;
+        }
+        
+        delay = 0;
+        for (int i = mFadeOutAnimationsList.length - 1; i >= 0; --i)
+        {          
+            mFadeOutAnimationsList[i].setFromValue(1.0f);
+            mFadeOutAnimationsList[i].setToValue(0.0f);
+            mFadeOutAnimationsList[i].setDelay(Duration.millis(delay));
+
+            delay += Constants.FAST_DELAY;
+        }
+    }
+
+    @FXML
+    private void onClickAddTests(ActionEvent event) 
+    {
+        // TODO
+    }
+
+    @FXML
+    private void onClickCompileTests(ActionEvent event) 
+    {
+        // TODO
+    }
+
+    @FXML
+    private void onClickRunTests(ActionEvent event) 
+    {
+        // TODO
+    }
+
+    @FXML
+    private void onClickSettings(ActionEvent event) 
+    {
+        // TODO
+    }
+
+    @FXML
+    private void onClickFAB(ActionEvent event) 
+    {        
+        if (mCommandsListVisible)
+        {            
+            for (int i = 0; i < mFadeOutAnimationsList.length; ++i)
+            {                
+                mFadeOutAnimationsList[i].play();
+            }
+        }
+        else
+        {
+            for (int i = 0; i < mFadeInAnimationsList.length; ++i)
+            {                
+                mFadeInAnimationsList[i].play();
+            }
+        }
+        
+        mCommandsListVisible = !mCommandsListVisible;
     }
 }
