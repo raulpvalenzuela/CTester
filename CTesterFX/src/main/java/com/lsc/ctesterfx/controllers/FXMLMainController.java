3be17fff1475a9babf6d29a59caef7bb4f448f30
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2019-06-04 17:09:51 +0200

    Unused import removed

--- CTesterFX/src/main/java/com/lsc/ctesterfx/controllers/FXMLMainController.java
+++ CTesterFX/src/main/java/com/lsc/ctesterfx/controllers/FXMLMainController.java
@@ -19,7 +19,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
