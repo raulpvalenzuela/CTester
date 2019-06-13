52c62b4e41daf40c3335aa681c295874434aae64
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2019-06-13 16:44:22 +0200

    CTesterAPI created and first configuration done

---
+++ CTesterAPI/src/main/java/com/lsc/ctesterapi/Logger.java
@@ -0,0 +1,40 @@
+package com.lsc.ctesterapi;
+
+import com.lsc.ctesterfx.logger.AbstractLogger;
+
+/**
+ *
+ * @author dma@logossmartcard.com
+ */
+public class Logger extends AbstractLogger
+{
+    @Override
+    public void log(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void logComment(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void logError(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void logWarning(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void logDebug(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void logSuccess(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
