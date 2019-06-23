c18db3135e364a9870424922399a9721d8be33ff
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2019-06-23 12:22:16 +0200

    Fixes #32

---
+++ CTester/CTesterAPI/src/main/java/com/lsc/ctesterapi/utls/ApduValidator.java
@@ -0,0 +1,38 @@
+package com.lsc.ctesterapi.utls;
+
+import org.apache.commons.codec.DecoderException;
+
+/**
+ *
+ * @author dma@logossmartcard.com
+ */
+public class ApduValidator
+{
+    /**
+     * Validates that the string is a valid apdu.
+     *
+     * @param command: command to be validated.
+     * @return true if it is a valid command, false otherwise.
+     */
+    public static boolean isValid(final String command)
+    {
+        String cmd = command.replaceAll(" ", "").trim();
+
+        // If it's less than 5 bytes.
+        if (cmd.length() < 5*2)
+        {
+            return false;
+        }
+
+        try
+        {
+            // Convert it to byte array, if an exception is thrown the command is not valid.
+            Formatter.fromStringToByteArray(cmd);
+
+        } catch (DecoderException ex) {
+            return false;
+        }
+
+        return true;
+    }
+}
